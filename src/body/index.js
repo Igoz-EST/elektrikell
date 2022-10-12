bf68451ea6e3f50915d1fc64ee16f7c13380163d
Igor_Kolpakchi <Igor.Kolpakchi.1999@gmail.com>
2022-10-12 18:22:40 +0300

    homework

---
+++ src/body/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+
+const BodyComponent = () => {
+ 
+return (
+        <Row></Row>   
+)
+}
+export default BodyComponent;
